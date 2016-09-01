7d63c80686fa90c20d303cb7f721e9bcd9b75ab2
Kriti Sen Sharma <ksen@paradigm4.com>
2016-09-01 15:28:34 +0000

    added example of cross product calculation

---
+++ cross_product.R
@@ -0,0 +1,23 @@
+library(scidb)
+scidbconnect()
+
+source('~/p4scratch/cross_product_scidb/functions.R')
+
+# Upload a 2D matrix to SciDB
+v1 <- as.scidb(matrix(rnorm(2500),50))
+
+# run the product in SciDB, and download the result
+mx = gemm(v1, v1)[]
+# The result is a dataframe, so we need to formulate a matrix from it
+mxmat = df_to_2dmat(mx, ordered = FALSE)
+
+##########
+# Now let us run the multiplication in R
+# Download the matrix as a dataframe
+v = v1[]
+# Convert from DF to matrix
+vmat = df_to_2dmat(v)
+m = vmat %*% vmat
+
+# Now compare the results
+all.equal(m, mxmat)
---
+++ functions.R
@@ -0,0 +1,64 @@
+gemm = function(X, Y, Z)
+{
+  if(missing(Z))
+  {
+    Z = scidb(sprintf("build(<val:double> [x=%s:%s,%s,0, y=%s:%s,%s,0], 0)", 
+                      scidb::scidb_coordinate_start(X)[1],
+                      scidb::scidb_coordinate_end(X)[1],
+                      scidb::scidb_coordinate_chunksize(X)[1],
+                      scidb::scidb_coordinate_start(Y)[2],
+                      scidb::scidb_coordinate_end(Y)[2],
+                      scidb::scidb_coordinate_chunksize(Y)[2]
+    ))
+  }
+  result = scidb(sprintf("gemm(%s, %s, %s)", X@name, Y@name, Z@name))
+  return(result)
+}
+
+# Convert a data-frame with three columns into a matrix
+# Column 1 has the matrix column name info
+# Column 2 has the matrix row name info
+# Column 3 has the matrix data
+# ordered: is the dataframe already ordered 
+df_to_2dmat <- function(df, ordered = TRUE)
+{
+  if (!ordered)
+  {
+    # Make sure the data frame is ordered exactly 
+    df = df[with(df, order(df[,1], df[,2])), ]
+  }
+  
+  ncols = length(unique(df[, 1]))
+  nrows = length(unique(df[, 2]))
+  mat = matrix(df[, 3], nrow=nrows, ncol=ncols)
+  #dim(mat)  
+  
+  #### Formulate the rownames and verify
+  rowlist = df[, 2]
+  rnames = unique(rowlist)
+  
+  if (ordered) {
+    # Verify
+    check = all.equal(rep(rnames, ncols), rowlist)
+    print(check)
+    stopifnot(check)
+  }
+  
+  #### Formulate the rownames and verify
+  collist = df[, 1]
+  cnames = unique(collist)
+  xx = unlist(lapply(cnames, function(x) {rep(x, nrows)}))
+  
+  if (ordered) {
+    # Verify
+    check = all.equal(xx, collist)
+    print(check)
+    stopifnot(check)
+  }
+  
+  # now paste the rownames and colnames to the matrix
+  rownames(mat) = rnames
+  colnames(mat) = cnames
+  
+  mat
+}
